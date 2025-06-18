18c4054cd140d358f709c49a978f5fe9eec3d78c
3v0k4 <riccardo.odone@gmail.com>
2025-06-18 09:27:06 +0200

    refactor: dry it up

--- lib/favicon_factory/cli.rb
+++ lib/favicon_factory/cli.rb
@@ -20,50 +20,46 @@ module FaviconFactory
     end
 
     def call
-      adapter = find_adapter.call
-      if adapter.nil?
-        stderr.puts "Error: Neither vips or imagemagick found, install one"
-        return 1
+      code, message = catch(:exit) do
+        adapter = try_find_adapter
+        params = try_parse(argv)
+        adapter.new(file: file, params: params, stderr: stderr).create_icons
+        [0, nil]
       end
 
-      params, status = parse(argv)
-      return status if status >= 0
-
-      adapter.new(file: file, params: params, stderr: stderr).create_icons
-      0
+      stderr.puts message unless message.nil?
+      code
     end
 
     private
 
     attr_reader :stderr, :file, :find_adapter, :argv
 
-    def parse(argv)
+    def try_find_adapter
+      adapter = find_adapter.call
+      throw(:exit, [1, "Error: Neither vips or imagemagick found, install one"]) if adapter.nil?
+      adapter
+    end
+
+    def try_parse(argv)
       command = Command.new.parse(argv)
       params = command.params
-      return exit_message(0, command.help) if params.fetch(:help) == true
-      return exit_message(1, params.errors.summary) if params.errors.any?
+      throw(:exit, [0, command.help]) if params.fetch(:help) == true
+      throw(:exit, [1, params.errors.summary]) if params.errors.any?
 
-      params = params.to_h
       favicon_svg = params.fetch(:favicon_svg)
-      return exit_message(1, "Error: #{favicon_svg} does not end with .svg") unless favicon_svg.end_with?(".svg")
-      return exit_message(1, "Error: #{favicon_svg} does not exist") unless file.exist?(favicon_svg)
+      throw(:exit, [1, "Error: #{favicon_svg} does not end with .svg"]) unless favicon_svg.end_with?(".svg")
+      throw(:exit, [1, "Error: #{favicon_svg} does not exist"]) unless file.exist?(favicon_svg)
 
       background = params.fetch(:background)
-      unless hex?(background)
-        return exit_message(1, "Error: #{background} is not a valid color, use a hex value like #0099ff")
-      end
+      throw(:exit, [1, "Error: #{background} is not a valid color, use a hex value like #0099ff"]) unless hex?(background)
 
-      [Params.new(favicon_svg: favicon_svg, background: background), -1]
+      Params.new(favicon_svg: favicon_svg, background: background)
     end
 
     def hex?(string)
       string = string.delete_prefix("#")
       string.chars.all? { |char| char.match?(/^[0-9a-fA-F]$/) }
-    end
-
-    def exit_message(status, message)
-      stderr.puts message
-      [nil, status]
     end
   end
 end
